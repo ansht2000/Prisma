606aac8479ccbaab52a046439a1129a0ae1266c4
Ansh Tarafder <ansht2000@gmail.com>
2024-10-18 14:41:12 -0700

    started implementing mirror class to draw simple 'mirrors' onto the screen

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+from mirror import Mirror
+
+def main():
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running  = True
+    dt = 0
+
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+    while running:
+        # poll for events
+        # pygame.QUIT represents the user pressing X to close the window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        # fill the screen with a color to wipe away anything from the last frame
+        screen.fill("black")
+
+        # Render game here
+        mirror = Mirror(screen.get_width() / 2, screen.get_height() / 2, 100)
+        mirror_2 = Mirror(screen.get_width() / 2 + 50, screen.get_height() / 2 + 50, 100, 180)
+        mirror.draw(screen)
+        mirror_2.draw(screen)
+
+        # flip() display to show the work done on the screen
+        pygame.display.flip()
+
+        # Limits the FPS to 60
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ mirror.py
@@ -0,0 +1,24 @@
+import pygame
+import math
+
+class Mirror(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, length=10, orientation=45):
+        super().__init__()
+        self.curved = False
+        # the x and y positions define the center of the line
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.length = length
+        self.orientation = orientation
+
+    def draw(self, screen):
+        radians = (math.pi * self.orientation) / 180
+        start_x = self.pos_x - (self.length / 2) * math.cos(radians)
+        start_y = self.pos_y + (self.length / 2) * math.sin(radians)
+        start_pos = pygame.Vector2(start_x, start_y)
+        end_pos = pygame.Vector2(start_x + self.length * math.cos(radians), start_y - self.length * math.sin(radians))
+        pygame.draw.line(screen, "white", start_pos, end_pos)
+
+    
+
+    
